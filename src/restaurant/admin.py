e8287916a29f2a27e17ca0fb599d531d547a1d70
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2023-04-20 21:05:12 +0200

    Added mysql database and some models

--- src/restaurant/admin.py
+++ src/restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Menu, Table
+
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(Table)
--- src/restaurant/models.py
+++ src/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+
+  name = models.CharField(max_length=256)
+  No_of_guest = models.IntegerField()
+  BookingDate = models.DateTimeField()
+
+
+class Table(models.Model):
+
+  Title = models.CharField(max_length=256)
+  Price = models.DecimalField(max_digits=10, decimal_places=2)
+  Inventory = models.IntegerField()
