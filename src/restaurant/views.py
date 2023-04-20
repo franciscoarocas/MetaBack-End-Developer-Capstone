9e35c9f7baac119496cad61d29f0b2e4fc637792
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2023-04-20 21:30:27 +0200

    Added serializers, rest framework and others

--- src/restaurant/views.py
+++ src/restaurant/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+
+from rest_framework.decorators import api_view
+from .models import Menu
+from .serializers import MenuItemSerializer
+
 # Create your views here.
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {})
+
+
+
+class MenuItemsView(ListCreateAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuItemSerializer
