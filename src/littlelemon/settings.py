840f149e545cb3ee848f18750af82c375c959f3c
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2023-04-22 16:48:44 +0200

    Update settings.py

--- src/littlelemon/settings.py
+++ src/littlelemon/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': 'e35l15s92',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
